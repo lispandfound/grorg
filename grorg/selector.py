4c9cdc13e4743d9265d8fb06ae8ebb564ed3ccb7
Jake Faulkner <jakefaulkn@gmail.com>
2017-05-24 21:28:38 +1200

    Implement absolute selectors

--- grorg/selector.py
+++ grorg/selector.py
@@ -5,17 +5,27 @@ import re
 class Selector:
     """ Class containing a path that specifies how to navigate the org-mode document. """
 
-    def __init__(self, selector, recursive=False):
+    def __init__(self, selector):
         """ Parse a document selector from a given string.
         Selectors are a path that specifies how to navigate the org-mode document. """
-        self.recursive = recursive
+        self.recursive = False
+        if selector[:2] == '//':
+            self.recursive = True
         self.path = [re.compile(part) for part in selector.strip('/').split('/')]
 
     @staticmethod
     def _header(node):
         return type(node) == PyOrgMode.OrgNode.Element
 
-    def _find_headers_matching(self, considered_nodes, part):
+    @staticmethod
+    def _expand(nodes):
+        children = []
+        for node in nodes:
+            if Selector._header(node):
+                children.extend(node.content)
+        return children
+
+    def _find_headers_matching(self, considered_nodes, part, recursive=False):
         """ Return a list of headers matching the regex part. """
         considering_nodes = considered_nodes[:]
         result = []
@@ -25,7 +35,7 @@ class Selector:
                 continue
             elif part.match(node.heading):
                 result.append(node)
-            else:
+            elif recursive:
                 considering_nodes.extend(node.content)
         return result
 
@@ -33,6 +43,10 @@ class Selector:
         """ Apply a selector to an org mode document. """
         considered_nodes = [document.root]
         for part in self.path:
+
+            considered_nodes = Selector._expand(considered_nodes)
+
             considered_nodes = self._find_headers_matching(considered_nodes,
-                                                           part)
+                                                           part,
+                                                           recursive=self.recursive)
         return considered_nodes
