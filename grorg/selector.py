2247fcbcbaf03126c0a5379167ae7e45869aabf2
Jake Faulkner <jakefaulkn@gmail.com>
2017-05-27 11:48:34 +1200

    Improve documentation for selector

--- grorg/selector.py
+++ grorg/selector.py
@@ -4,8 +4,9 @@ import re
 
 
 def find_headers_matching(node, predicate, recursive=False):
-    """ Return a list of all sub-nodes of node (or list of nodes) matching a predicate.
-    If the recursive flag is set, consider all sub-nodes recursively"""
+    """ Return a list of all sub-nodes of node (or list of nodes)
+    matching a predicate. If the recursive flag is set, consider all
+    sub-nodes recursively. """
     if type(node) == list:
         node_stack = deque(node)
     else:
@@ -23,11 +24,13 @@ def find_headers_matching(node, predicate, recursive=False):
 
 
 class Selector:
-    """ Class containing a path that specifies how to navigate the org-mode document. """
+    """ Class containing a path that specifies how to navigate the
+    org-mode document. """
 
     def __init__(self, selector):
         """ Parse a document selector from a given string.
-        Selectors are a path that specifies how to navigate the org-mode document. """
+        Selectors are a path that specifies how to navigate the
+        org-mode document. """
         self.recursive = False
         if selector[:2] == '//':
             self.recursive = True
@@ -40,7 +43,8 @@ class Selector:
 
     @staticmethod
     def _expand(nodes):
-        """ Return a list containing all the children of every node in nodes. """
+        """ Return a list containing all the children of every node in
+        nodes. """
         children = []
         for node in nodes:
             if Selector._header(node):
