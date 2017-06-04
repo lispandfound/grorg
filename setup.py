1ae506be6f85057d4bf831d09b0307a15538a106
Jake Faulkner <jakefaulkn@gmail.com>
2017-06-04 21:32:23 +1200

    CLI actually installs now

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="grorg",
     version='0.1',
     py_modules=['grorg'],
+    packages=find_packages(),
     install_requires=[
         'Click',
+        'PyOrgMode'
     ],
     entry_points='''
         [console_scripts]
-        grorg=main:cli
+        grorg = grorg.grorg:cli
     ''',
 )
